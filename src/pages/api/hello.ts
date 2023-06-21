38793c1b9adeb768179591fed75e5a17c6712f62
shoxie <19520377@gm.uit.edu.vn>
2023-06-21 18:17:27 +0700

    cheat

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -21,13 +21,16 @@ export default async function handler(
     //     uid: "b99770aa"
     //   }
     // })
+    const students = await prisma.user.findMany()
+
     const demoClass = await prisma.class.create({
       data: {
         day: 1,
         endTime: "9:00 PM",
         startTime: "7:00 AM",
         name: "LMAO001",
-        subject: "OOP"
+        subject: "OOP",
+        studentIds: students.map(item => item.id)
       }
     })
     res.status(200).json({ message: "ok"})
